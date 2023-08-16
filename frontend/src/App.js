4e924dec9fab54aab8eea54c4875a17268604b20
mahisami <mahletsamueldon@gmail.com>
2023-08-15 22:10:40 -0700

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/home";
+import About from "./components/About";
+
+export default function App() {
+  return (
+
+    <div className="content">
+      
+      <Home />
+      <About/>
+
+    </div>
+  );
+
+}
---
+++ frontend/src/components/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/about.css";
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="about-description">
+                <h2>About Us</h2>
+
+                <p >
+                    Welcome to our website! We are a passionate team dedicated to providing<br />
+                    the best services in our industry. Our mission is to deliver exceptional<br />
+                    experiences to our customers, ensuring their satisfaction and success.
+                </p>
+            </div>
+            <div className="about-details">
+                <h3>Our Team</h3>
+                <p>
+                    Meet the people behind our success. Our talented and motivated team<br />
+                    works tirelessly to bring innovation and quality to everything we do.mmmmmm<br />
+                    Together, we strive to exceed expectations and make a positive impact.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
