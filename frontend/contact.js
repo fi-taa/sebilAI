01a0c4c6406065ea17dc5f21526d0fea0916875d
BettyZak <101482378+BettyZak@users.noreply.github.com>
2023-08-22 16:04:42 +0300

    Update contact.js

--- frontend/contact.js
+++ frontend/contact.js
@@ -7,7 +7,7 @@ function ContactForm() {
  
   const handleSubmit = (e) => { 
     e.preventDefault(); 
-    // Handle form submission logic here 
+    
     console.log('Form submitted:', { name, email, message }); 
   }; 
  
